a530f2aa878d35825103d08491d4274d382cb51f
ammarrashed <ammarrashed54@gmail.com>
2023-03-23 13:25:38 -0400

    camera feed added in div

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template, Response
+
+from scripts.feed import get_webcam_feed
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def index():  # put application's code here
+    return render_template("index.html")
+
+
+@app.route("/video")
+def video():
+    return Response(get_webcam_feed(), mimetype="multipart/x-mixed-replace; boundary=frame")
 
 
 if __name__ == '__main__':
